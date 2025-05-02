c738d1c1c3f2f748f36fb282e33f527d527157a1
enbolstad <eirik.bols@outlook.com>
2025-05-02 13:32:58 +0200

    Update providers.tf

---
+++ modules/adme_public_endpoints/providers.tf
@@ -0,0 +1,11 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.22.0"
+    }
+
+    azapi = {
+      source  = "Azure/azapi"
+      version = "2.0.1"
+    }
