abd5f9c98af88bb490b7c86095f6efab97a88b3f
Eirik Bolstad <eirik@like-it.no>
2025-05-02 20:51:31 +0200

    1

--- modules/adme_public_endpoints/adme_public_endpoints.tf
+++ modules/adme_public_endpoints/adme_public_endpoints.tf
@@ -4,53 +4,6 @@ resource "azurerm_resource_group" "default" {
   location = var.location
 }
 
-resource "azurerm_resource_group_template_deployment" "default" {
-  name                = var.adme_name
-  resource_group_name = azurerm_resource_group.default.name
-  deployment_mode     = "Incremental"
-  parameters_content = jsonencode({
-    "name" = {
-    value = "${var.adme_name}" }
-    "location" = {
-    value = "${var.location}" }
-    "tagsByResource" = {
-      value = var.tags
-    }
-    "authAppId" = {
-      value = "${var.authAppId}"
-    }
-    "dataPartitionNames" = {
-      value = [
-        {
-          "name" = "${var.adme_datapartition_name}"
-        }
-      ]
-    }
-    "cmkEnabled" = {
-    value = false }
-    "encryption" = {
-    value = {} }
-    "identity" = {
-    value = {} }
-    "corsRules" = {
-    value = [] }
-    "sku" = {
-      value = {
-        "name" = "${var.adme_sku}"
-      }
-    }
-    "publicNetworkAccess" = {
-    value = true }
-    "privateEndpoints" = {
-      value = []
-    }
-    "resourceGroupId" = {
-    value = "/subscriptions/${var.subscription_id}/resourceGroups/${var.rg_name}" }
-  })
-
-  template_content = file("template.json")
-}
-
 resource "azapi_resource" "adme" {
   type                      = "Microsoft.OpenEnergyPlatform/energyServices@2023-02-21-preview"
   name                      = var.adme_name
