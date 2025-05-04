cc8537148d3f551b6f8fcbe235a0c68134c86aab
Eirik Bolstad <eirik@like-it.no>
2025-05-04 10:59:57 +0200

    1

--- modules/adme_public_endpoints/adme_public_endpoints.tf
+++ modules/adme_public_endpoints/adme_public_endpoints.tf
@@ -5,25 +5,38 @@ resource "azurerm_resource_group" "default" {
 }
 
 resource "azapi_resource" "adme" {
+  type                      = "Microsoft.OpenEnergyPlatform/energyServices@2023-02-21-preview"
+  name                      = "soprabp777"
+  location                  = "northeurope"
+  ignore_missing_property   = true
+  schema_validation_enabled = false
+
+  timeouts {
+    create = "5h30m"
+    update = "5h30m"
+    delete = "5h30m"
+  }
+
+  parent_id = azurerm_resource_group.default.id
+
   body = {
     properties = {
       authAppId = var.authAppId
-      dataPartitionNames = [{
-        name = "preprod"
+      dataPartitionNames = [
+        {
+          name = "preprod"
         },
         {
           name = "preprod2"
-      }]
+        }
+      ]
+
+      sku = {
+        name = var.adme_sku
+        tier = "Standard"
+      }
+
+      publicNetworkAccess = "Enabled"
     }
   }
-  ignore_casing             = false
-  ignore_missing_property   = true
-  location                  = "northeurope"
-  name                      = "soprabp777"
-  parent_id                 = azurerm_resource_group.default.id
-  schema_validation_enabled = true
-  type                      = "Microsoft.OpenEnergyPlatform/energyServices@2022-04-04-preview"
-  depends_on = [
-    azurerm_resource_group.default
-  ]
 }
