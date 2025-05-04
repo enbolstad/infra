ac5ae92c8de237277ae736b9e131bc13cfb3a1d2
Eirik Bolstad <eirik@like-it.no>
2025-05-04 10:05:10 +0200

    1

--- modules/adme_public_endpoints/adme_public_endpoints.tf
+++ modules/adme_public_endpoints/adme_public_endpoints.tf
@@ -4,27 +4,37 @@ resource "azurerm_resource_group" "default" {
   location = var.location
 }
 
-resource "azapi_resource" "adme" {
+resource "azapi_resource" "adme-osdu-dev" {
+  type     = "Microsoft.OpenEnergyPlatform/energyServices@2023-02-21-preview"
+  name     = "soprabp777"
+  location = "northeurope"
+  timeouts {
+    create = "5h30m"
+    update = "5h30m"
+    delete = "5h30m"
+  }
+  tags = {
+    Owner       = "OSDU Platform Team"
+    Project     = "OSDU"
+    Environment = "dev"
+  }
+  parent_id                 = azurerm_resource_group.default
+  schema_validation_enabled = false
   body = {
     properties = {
-      authAppId = "f37be710-de99-4d1d-bc62-8f5cde53d030"
-      dataPartitionNames = [{
-        name = "preprod"
-        }, {
-        name = "dp3"
-        }, {
-        name = "dp4"
-      }]
+      authAppId = var.authAppId
+      dataPartitionNames = [
+        {
+          name = "preprod"
+        }
+      ]
+
+      sku = {
+        name = "Developer"
+        tier = "Standard"
+      }
+
+      publicNetworkAccess = "Enabled"
     }
   }
-  ignore_casing             = false
-  ignore_missing_property   = true
-  location                  = "northeurope"
-  name                      = "soprabp85"
-  parent_id                 = "/subscriptions/464c8a03-5870-4a25-94f6-69cc795997ed/resourceGroups/rg-msa-app-adme-prod-we-001"
-  schema_validation_enabled = true
-  type                      = "Microsoft.OpenEnergyPlatform/energyServices@2022-04-04-preview"
-  depends_on = [
-    azurerm_resource_group.default
-  ]
 }
